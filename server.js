9f78722dd0d607cdd0ef55a0d33b3fbc519127e6
Vince <vincentw@jacquieetmichel.fr>
2023-10-29 17:14:57 +0100

    edit server

--- server.js
+++ server.js
@@ -1,72 +1,52 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
 
-
-const PORT = process.env.PORT || 3000;
-
-// Middleware pour parser les données POST en JSON
 app.use(express.json());
 
+// Définir le chemin du fichier JSON
+const jsonFilePath = path.join(__dirname, 'responses.json');
 
+// Endpoint pour obtenir les réponses en tant qu'objet JSON
+app.get('/responses', (req, res) => {
+    try {
+        const jsonData = fs.readFileSync(jsonFilePath, 'utf8');
+        const responses = JSON.parse(jsonData);
+        res.json(responses);
+    } catch (error) {
+        console.error('Erreur lors de la lecture du fichier JSON :', error.message);
+        res.status(500).json({ error: 'Erreur lors de la lecture des réponses.' });
+    }
+});
+
+// Endpoint pour soumettre un formulaire
+app.post('/submit', (req, res) => {
+    try {
+        const formData = req.body;
+        // Faites quelque chose avec les données, par exemple, enregistrez-les dans le fichier JSON
+        // Enregistrez la nouvelle réponse dans le fichier JSON
+        const jsonData = fs.readFileSync(jsonFilePath, 'utf8');
+        const responses = JSON.parse(jsonData);
+        responses.push(formData);
+        fs.writeFileSync(jsonFilePath, JSON.stringify(responses, null, 2), 'utf8');
+        res.status(200).json({ message: 'Formulaire soumis avec succès!' });
+    } catch (error) {
+        console.error('Erreur lors de l\'enregistrement de la réponse :', error.message);
+        res.status(500).json({ error: 'Erreur lors de l\'enregistrement de la réponse.' });
+    }
+});
+
+// Définir le dossier contenant les fichiers statiques (comme index.html)
 app.use(express.static(path.join(__dirname, '/')));
 
-app.use(bodyParser.json());
-
-
-app.post('/submit', (req, res) => {
-    const response = req.body;
-
-    // Charger les anciennes réponses
-    let responses = [];
-    try {
-        responses = JSON.parse(fs.readFileSync('responses.json'));
-    } catch (error) {
-        console.error(error);
-    }
-
-    // Ajouter la nouvelle réponse
-    responses.push(response);
-
-    // Enregistrer les réponses dans le fichier JSON
-    fs.writeFileSync('responses.json', JSON.stringify(responses, null, 2));
-
-    res.json({ success: true });
-    res.status(200).json({ message: 'Formulaire soumis avec succès!' });
+// Route pour la page d'accueil
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/responses', (req, res) => {
-    // Charger les réponses depuis le fichier JSON
-    let responses = [];
-    try {
-        responses = JSON.parse(fs.readFileSync('responses.json'));
-    } catch (error) {
-        console.error(error);
-    }
-
-    // Filtrer et trier les réponses par rôle et classe
-    let sortedResponses = {
-        tank: [],
-        heal: [],
-        'dps-distance': [],
-        'dps-cac': []
-    };
-
-    responses.forEach(response => {
-        sortedResponses[response.role].push(response);
-    });
-
-    res.json(sortedResponses);
-});
-
-app.get('/results', (req, res) => {
-    res.sendFile(path.join(__dirname, 'results.html'));
-});
-
-
-app.listen(port, () => {
-    console.log(`Serveur en écoute sur le port ${port}`);
+// Écouter le serveur sur le port 3000 (ou le port spécifié dans l'environnement)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur en écoute sur le port ${PORT}`);
 });
