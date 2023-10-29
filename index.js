5ae9548ada656cf470323be4dbb7299f20874665
lefourbefromage <vincent.waldmann@gmail.com>
2023-10-29 19:22:04 +0100

    azea

--- index.js
+++ index.js
@@ -42,16 +42,12 @@ app.post('/submit', (req, res) => {
         res.status(200).json({ message: 'Formulaire soumis avec succès!' });
 });
 
-// Définir le dossier contenant les fichiers statiques (comme index.html)
-app.use(express.static(path.join(__dirname, '/')));
-
-// Route pour la page d'accueil
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/results', (req, res) => {
-    res.sendFile(path.join(__dirname, 'results.html'));
+    res.sendFile(path.join(__dirname, 'public/results.html'));
 });
 
 module.exports = app;
